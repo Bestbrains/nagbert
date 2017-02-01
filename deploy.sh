0bb7ae2c77345d7c8e2a9c2d63efe9b202a7abf0
Sonny Korte <sonny.korte@bestbrains.dk>
2017-02-01 17:11:30 +0100

    travis debugging

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,18 @@
 if [ ! -d "dist" ]; then
   mkdir dist
 fi
-#: ${deploytarget:=nagbert}
+: ${deploytarget:=nagbert}
 
-#tar -zcf dist/nagbert.tar.gz node_modules index.js package.json
+tar -zcf dist/nagbert.tar.gz node_modules index.js package.json
 echo 'Wrote package dist/nagbert.tar.gz'
 
-#scp dist/nagbert.tar.gz ${deploytarget}:.
-#scp nagbert.service ${deploytarget}:.
+scp dist/nagbert.tar.gz ${deploytarget}:.
+scp nagbert.service ${deploytarget}:.
 echo 'Copied tarball to destination'
 
-#ssh -X ${deploytarget} 'mkdir -p nagbert && tar -xzf nagbert.tar.gz -C nagbert'
-#ssh -X ${deploytarget} 'sudo systemctl stop nagbert.service'
-#ssh -X ${deploytarget} 'sudo cp nagbert.service /etc/systemd/system/nagbert.service'
-#ssh -X ${deploytarget} 'sudo systemctl start nagbert.service'
+ssh -X ${deploytarget} 'mkdir -p nagbert && tar -xzf nagbert.tar.gz -C nagbert'
+ssh -X ${deploytarget} 'sudo systemctl stop nagbert.service'
+ssh -X ${deploytarget} 'sudo cp nagbert.service /etc/systemd/system/nagbert.service'
+ssh -X ${deploytarget} 'sudo systemctl start nagbert.service'
 
 echo 'Installed service nagbert'
