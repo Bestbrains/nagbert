600b248ba930be4e469715caddd18a0e3c843b82
Sonny Korte <sonny.korte@bestbrains.dk>
2017-02-01 15:44:57 +0100

    Deploy using ssh and the encrypted keys

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,18 @@
 if [ ! -d "dist" ]; then
   mkdir dist
 fi
+: ${deploytarget:=nagbert}
 
 tar -zcf dist/nagbert.tar.gz node_modules index.js package.json
 echo 'Wrote package dist/nagbert.tar.gz'
 
-scp dist/nagbert.tar.gz nagbert:.
-scp nagbert.service nagbert:.
+scp dist/nagbert.tar.gz ${deploytarget}:.
+scp nagbert.service ${deploytarget}:.
 echo 'Copied tarball to destination'
 
-ssh -X nagbert 'mkdir -p nagbert && tar -xzf nagbert.tar.gz -C nagbert'
-ssh -X nagbert 'sudo systemctl stop nagbert.service'
-ssh -X nagbert 'sudo cp nagbert.service /etc/systemd/system/nagbert.service'
-ssh -X nagbert 'sudo systemctl start nagbert.service'
+ssh -X ${deploytarget} 'mkdir -p nagbert && tar -xzf nagbert.tar.gz -C nagbert'
+ssh -X ${deploytarget} 'sudo systemctl stop nagbert.service'
+ssh -X ${deploytarget} 'sudo cp nagbert.service /etc/systemd/system/nagbert.service'
+ssh -X ${deploytarget} 'sudo systemctl start nagbert.service'
 
 echo 'Installed service nagbert'
