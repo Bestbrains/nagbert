bc4d8afb86971ce1b21edb03eb360582fd9f9b12
Sonny Korte <sonny.korte@bestbrains.dk>
2017-02-08 15:11:09 +0100

    show output from scp to verify that the ip is added to known_hosts

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,13 @@ fi
 tar -zcf dist/nagbert.tar.gz node_modules index.js package.json _info.json
 echo 'Wrote package dist/nagbert.tar.gz'
 
-scp dist/nagbert.tar.gz ${deploytarget}:. >> deploy.log
-scp nagbert.service ${deploytarget}:. >> deploy.log
+scp dist/nagbert.tar.gz ${deploytarget}:.
+scp nagbert.service ${deploytarget}:.
 echo 'Copied tarball to destination'
 
-ssh -X ${deploytarget} 'mkdir -p nagbert && tar -xzf nagbert.tar.gz -C nagbert' >> deploy.log
-ssh -X ${deploytarget} 'sudo systemctl stop nagbert.service' >> deploy.log
-ssh -X ${deploytarget} 'sudo cp nagbert.service /etc/systemd/system/nagbert.service' >> deploy.log
-ssh -X ${deploytarget} 'sudo systemctl start nagbert.service' >> deploy.log
+ssh -X ${deploytarget} 'mkdir -p nagbert && tar -xzf nagbert.tar.gz -C nagbert'
+ssh -X ${deploytarget} 'sudo systemctl stop nagbert.service'
+ssh -X ${deploytarget} 'sudo cp nagbert.service /etc/systemd/system/nagbert.service'
+ssh -X ${deploytarget} 'sudo systemctl start nagbert.service'
 
 echo 'Installed service nagbert'
-scp deploy.log ${deploytarget}:.
